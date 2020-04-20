376a631f6d83d3d1b22309c7b6841a6a2f25cb9a
Lino Le Van <lino.levan@gmail.com>
2020-04-20 08:12:15 -0700

    First upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Albanian_Virus
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            var result = MessageBox.Show("Hi, I am an Albanian virus but because of poor technology in my country, unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards, Albanian virus","Virus Alert", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
+            if(result == DialogResult.No){
+                MessageBox.Show("You are very mean","Virus Alert", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if(result == DialogResult.Yes){
+                MessageBox.Show("Thank you very much","Virus Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+	}
+}
