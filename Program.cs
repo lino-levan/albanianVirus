a32707da1a82fde5fa89d821bdf3a18a7c073249
Lino Le Van <lino.levan@gmail.com>
2020-04-20 08:12:48 -0700

    Fixed Indentation

--- Program.cs
+++ Program.cs
@@ -11,13 +11,13 @@ namespace Albanian_Virus
 	{
 		static void Main(string[] args)
 		{
-            var result = MessageBox.Show("Hi, I am an Albanian virus but because of poor technology in my country, unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards, Albanian virus","Virus Alert", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
-            if(result == DialogResult.No){
-                MessageBox.Show("You are very mean","Virus Alert", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            if(result == DialogResult.Yes){
-                MessageBox.Show("Thank you very much","Virus Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-        }
+		    var result = MessageBox.Show("Hi, I am an Albanian virus but because of poor technology in my country, unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards, Albanian virus","Virus Alert", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
+		    if(result == DialogResult.No){
+			MessageBox.Show("You are very mean","Virus Alert", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		    }
+		    if(result == DialogResult.Yes){
+			MessageBox.Show("Thank you very much","Virus Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		    }
+        	}
 	}
 }
